b4bec0d7c944bd4324fd8be17733c1378cece020
Lucas Gaston Ferreyra <lucas.ferreyrabernasconi@gmail.com>
2023-07-03 21:42:12 -0300

    clases Persona y Estudiante

--- Clase08/UniversidadApp/src/ar/digitalers/domain/Persona.java
+++ Clase08/UniversidadApp/src/ar/digitalers/domain/Persona.java
@@ -1,4 +1,50 @@
 package ar.digitalers.domain;
 
+
 public class Persona {
+    private String nombre;
+    private String apellido;
+    private int dni;
+    private String direccion;
+    private String telefono;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
